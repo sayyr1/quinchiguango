0a9e84b7caaf5bccae6b9efe543893a3acfa394c
sayyr1 <edwinsay03@gmail.com>
2020-12-16 00:14:00 -0500

    do it

--- Scoot/js/dates.js
+++ Scoot/js/dates.js
@@ -1,9 +1,2 @@
 let lastMod= new Date(document.lastModified);
 document.getElementById('dates').textContent=lastMod;
-WebFont.load({
-    google: {
-      families: [
-         'Roboto'
-      ]
-    }
-  });
