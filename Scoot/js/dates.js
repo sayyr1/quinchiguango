3705f5defafdd78c07cda2f121db129bc1aa3752
sayyr1 <edwinsay03@gmail.com>
2020-12-17 07:52:52 -0500

    dates

--- Scoot/js/dates.js
+++ Scoot/js/dates.js
@@ -1,2 +1,53 @@
+//Hamburguer buttom
+function toggleMenu(){
+    document.getElementById('primaryNav').classList.toggle('hide');   
+    }
+    const imagesToLoad= document.querySelectorAll('img[data-src]');
+    imgOptions ={
+        threshold:0,
+        rootMargin:"0px 0px 50px 0px"
+    
+    }
+    
+    
+    
+    
+    const loadImages=(image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () =>{
+        image.removeAttribute('data-src');
+    
+    };
+    
+    };
+    if('IntersectionObserver' in window){
+        const imgObserver = new IntersectionObserver((items, imgObserver) =>{
+        items.forEach((item)=>{
+            if(item.isIntersecting){
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+    
+    
+            }
+    
+        });
+    
+    
+    
+        }, imgOptions);
+        imagesToLoad.forEach((img) =>{
+            imgObserver.observe(img);
+        });
+    
+    }
+    else{
+        imagesToLoad.forEach((img) =>{
+            loadImages(img);
+        });
+    
+    
+    }
+
+
 let lastMod= new Date(document.lastModified);
 document.getElementById('dates').textContent=lastMod;
